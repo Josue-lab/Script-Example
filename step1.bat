bef3433223643ff6656b17cd29773b03c08d36f5
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-03-31 20:29:58 -0700

    Create step1.bat

---
+++ step1.bat
@@ -0,0 +1,12 @@
+mkdir "Step 1"
+echo "To make directory Step 1"
+cd "Step 1"
+echo "Change directory to Step 1"
+
+cat "Hello World" >> Hello.txt
+echo " Creating Hello file"
+
+cd .. 
+
+rmdir "Step 1"
+echo "Remove directory Step 1"
