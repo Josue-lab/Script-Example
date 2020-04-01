c25f88f563b34183b0f217e89e6c73b6af304192
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-04-01 01:29:18 -0700

    Update step2.bat

--- step2.bat
+++ step2.bat
@@ -1,9 +1,10 @@
-FILE="C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\"
-if [ -d "$FILE" ]; then 
-   echo "$FILE is a directory"
-else 
+IF EXIST "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1" (
+   REM echo "Directory created"
+)
+   ELSE ( 
    cd $FILE
    git add .
    git commit -m "Commit"
    git push origin master
-fi
+)
+
