997c71a5da4897682313a8b5b65ca1047f105831
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-03-31 22:58:22 -0700

    Update step2.bat

--- step2.bat
+++ step2.bat
@@ -1,3 +1,1 @@
-git add C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1\file1.txt
-git commit -m "Commit"
-git remote add origin https://github.com/Josue-lab/Script-Example.git
+gradlew init
