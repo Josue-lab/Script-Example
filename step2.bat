9d671660f4478db12d6c1167caf03d4aeb77eb59
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-04-01 01:36:16 -0700

    Update step2.bat

--- step2.bat
+++ step2.bat
@@ -1,10 +1,11 @@
 IF EXIST "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1" (
    echo "Directory created"
-)
-   ELSE ( 
    cd "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1"
    git add .
    git commit -m "Commit"
    git push origin master
+   else (
+   echo "Directory was not created"
+   )  
 )
 
