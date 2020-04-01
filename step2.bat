a8983b4b30bf3462d54f86b7d557bf7ec005a35e
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-03-31 22:52:11 -0700

    Update step2.bat

--- step2.bat
+++ step2.bat
@@ -1,4 +1,3 @@
-cd "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1"
-git add . 
+git add "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1\file1.txt"
 git commit -m "Commit"
 git remote add origin https://github.com/Josue-lab/Script-Example.git
