29a400ef29ae8cf1b75b197adf4393a99b7f16cc
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-04-01 01:33:37 -0700

    Update step2.bat

--- step2.bat
+++ step2.bat
@@ -1,8 +1,8 @@
 IF EXIST "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1" (
-   REM echo "Directory created"
+   echo "Directory created"
 )
    ELSE ( 
-   cd $FILE
+   cd "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1"
    git add .
    git commit -m "Commit"
    git push origin master
