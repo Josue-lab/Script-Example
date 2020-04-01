724a63f62be365af3848cb129930d8cdc47c9fc3
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-04-01 01:14:47 -0700

    Update step2.bat

--- step2.bat
+++ step2.bat
@@ -1,4 +1,10 @@
-cd "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1"
-git add .
-git commit -m "Commit"
-git push origin master
+FILE="C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1"
+
+if [ -d "$FILE" ]; then 
+   echo "$File is a directory"
+else 
+   cd "C:\Users\josue.valencia\.jenkins\workspace\Pipelines_123\ScripPipeline\Step 1"
+   git add .
+   git commit -m "Commit"
+   git push origin master
+fi
